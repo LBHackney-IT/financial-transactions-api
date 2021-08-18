aa294989d58acb35122bfd6c01c98ff8c690bb45
Anna Golosova <golosowa.anna@gmail.com>
2021-08-18 15:32:38 +0300

    Update terraform

---
+++ terraform/staging/dynamodb.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "transactions_table" {
+    name                  = "Transactions"
+    billing_mode          = "PROVISIONED"
+    read_capacity         = 10
+    write_capacity        = 10
+    hash_key              = "id"
+
+    attribute {
+        name              = "id"
+        type              = "S"
+    }
+
+    tags = {
+        Name              = "financial-transaction-${var.environment_name}"
+        Environment       = var.environment_name
+        terraform-managed = true
+        project_name      = var.project_name
+    }
+
+    point_in_time_recovery {
+        enabled           = true
+    }
+}
