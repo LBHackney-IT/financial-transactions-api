bc98f1369255a8e858a23b5011bebfd6eea07a52
Anna Golosova <golosowa.anna@gmail.com>
2021-08-18 15:40:40 +0300

    Update naming of api

--- terraform/staging/dynamodb.tf
+++ terraform/staging/dynamodb.tf
@@ -11,7 +11,7 @@ resource "aws_dynamodb_table" "transactions_table" {
     }
 
     tags = {
-        Name              = "financial-transaction-${var.environment_name}"
+        Name              = "financial-transactions-api-${var.environment_name}"
         Environment       = var.environment_name
         terraform-managed = true
         project_name      = var.project_name
