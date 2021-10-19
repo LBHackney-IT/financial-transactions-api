4ca1d4a4828f628dc4549592916d41c629ba5db1
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-10-19 11:13:35 +0330

    es-configuration

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -49,3 +49,43 @@ resource "aws_ssm_parameter" "new_financial-transactions_created_sns_arn" {
     type  = "String"
     value = aws_sns_topic.financial-transactions_topic.arn
 }
+
+/*    ELASTICSEARCH SETUP    */
+
+data "aws_vpc" "development_vpc" {
+  tags = {
+    Name = "vpc-housing-development"
+  }
+}
+
+data "aws_subnet_ids" "development" {
+  vpc_id = data.aws_vpc.development_vpc.id
+  filter {
+    name   = "tag:Type"
+    values = ["private"]
+  }
+}
+
+module "elasticsearch_db_development" {
+  source           = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/database/elasticsearch"
+  vpc_id           = data.aws_vpc.development_vpc.id
+  environment_name = "development"
+  port             = 443
+  domain_name      = "financial-transaction-api-es"
+  subnet_ids       = [tolist(data.aws_subnet_ids.development.ids)[0]]
+  project_name     = "financial-transaction"
+  es_version       = "7.8"
+  encrypt_at_rest  = "true"
+  instance_type    = "t3.small.elasticsearch"
+  instance_count   = "1"
+  ebs_enabled      = "true"
+  ebs_volume_size  = "10"
+  region           = data.aws_region.current.name
+  account_id       = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_ssm_parameter" "search_elasticsearch_domain" {
+  name = "/housing-search-api/development/elasticsearch-domain"
+  type = "String"
+  value = "https://vpc-housing-search-api-es-klp5oycl6thlxaub2mzu5zlj5u.eu-west-2.es.amazonaws.com"  
+}
