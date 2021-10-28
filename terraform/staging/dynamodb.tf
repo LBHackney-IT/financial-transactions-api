dd39436859d28f04a8c8b13081c7a1f9d67f1533
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-10-28 15:09:30 +0330

    adding new GSI & Fixing format

--- terraform/staging/dynamodb.tf
+++ terraform/staging/dynamodb.tf
@@ -10,11 +10,60 @@ resource "aws_dynamodb_table" "transactions_table" {
         type              = "S"
     }
 
+    attribute {
+        name              = "target_id"
+        type              = "S"
+    }
+
     tags = {
         Name              = "financial-transactions-api-${var.environment_name}"
         Environment       = var.environment_name
         terraform-managed = true
         project_name      = var.project_name
+    }
+
+    global_secondary_index {
+        name               = "target_id_dx"
+        hash_key           = "target_id"
+        write_capacity     = 10
+        read_capacity      = 10
+        projection_type    = "ALL"
+    }
+
+    point_in_time_recovery {
+        enabled           = true
+    }
+}
+resource "aws_dynamodb_table" "transactions_table" {
+    name                  = "Transactions"
+    billing_mode          = "PROVISIONED"
+    read_capacity         = 10
+    write_capacity        = 10
+    hash_key              = "id"
+
+    attribute {
+        name              = "id"
+        type              = "S"
+    }
+
+    attribute {
+        name              = "target_id"
+        type              = "S"
+    }
+
+    tags = {
+        Name              = "financial-transactions-api-${var.environment_name}"
+        Environment       = var.environment_name
+        terraform-managed = true
+        project_name      = var.project_name
+    }
+
+    global_secondary_index {
+        name               = "target_id_dx"
+        hash_key           = "target_id"
+        write_capacity     = 10
+        read_capacity      = 10
+        projection_type    = "ALL"
     }
 
     point_in_time_recovery {
