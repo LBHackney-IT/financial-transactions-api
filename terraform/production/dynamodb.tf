6671626688e754a16429e60902553d4abf496f26
AnnaGolosova <golosowa.anna@gmail.com>
2021-12-17 21:36:12 +0300

    Remove GSI and unnesessary attributes definition for staging and productions terraform scripts

--- terraform/production/dynamodb.tf
+++ terraform/production/dynamodb.tf
@@ -3,7 +3,8 @@ resource "aws_dynamodb_table" "transactions_table" {
     billing_mode          = "PROVISIONED"
     read_capacity         = 10
     write_capacity        = 10
-    hash_key              = "id"
+    hash_key              = "target_id"
+    range_key             = "id"
 
     attribute {
         name              = "id"
@@ -15,45 +16,11 @@ resource "aws_dynamodb_table" "transactions_table" {
         type              = "S"
     }
 
-    attribute {
-        name              = "transaction_type"
-        type              = "S"
-    }
-
-    attribute {
-        name              = "is_suspense"
-        type              = "S"
-    }    
-
     tags = {
         Name              = "financial-transactions-api-${var.environment_name}"
         Environment       = var.environment_name
         terraform-managed = true
         project_name      = var.project_name
-    }
-
-    global_secondary_index {
-        name               = "target_id_dx"
-        hash_key           = "target_id"
-        write_capacity     = 10
-        read_capacity      = 10
-        projection_type    = "ALL"
-    }
-
-    global_secondary_index {
-        name               = "transaction_type_dx"
-        hash_key           = "transaction_type"
-        write_capacity     = 10
-        read_capacity      = 10
-        projection_type    = "ALL"
-    }
-
-    global_secondary_index {
-        name               = "is_suspense_dx"
-        hash_key           = "is_suspense"
-        write_capacity     = 10
-        read_capacity      = 10
-        projection_type    = "ALL"
     }
 
     point_in_time_recovery {
