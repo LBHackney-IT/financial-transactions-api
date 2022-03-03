bc23d9aae5f193575b590ad0eae9439232a85447
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2022-03-03 20:04:14 +0330

    Auto scaling changes

--- terraform/staging/dynamodb.tf
+++ terraform/staging/dynamodb.tf
@@ -27,3 +27,28 @@ resource "aws_dynamodb_table" "transactions_table" {
         enabled           = true
     }
 }
+
+resource "aws_appautoscaling_target" "dynamodb_table_write_target" {
+  max_capacity       = 2000
+  min_capacity       = 100
+  resource_id        = "table/${aws_dynamodb_table.transactions_table.name}"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "dynamodb_table_write_policy" {
+  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_write_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.dynamodb_table_write_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.dynamodb_table_write_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.dynamodb_table_write_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+
+    target_value = 70
+  }
+}
+
