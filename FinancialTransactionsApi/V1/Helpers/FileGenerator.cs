878c8b57a094a8bf21f251d9ab32e9a0a54bafa6
DESKTOP-KJNO7KJ\Ilko Adamov <ilko.z.adamov@gmail.com>
2022-10-25 14:22:58 +0300

    HFS/584-Gateway-implementation-transactions-targetid - sonar cloud issues fix

--- FinancialTransactionsApi/V1/Helpers/FileGenerator.cs
+++ FinancialTransactionsApi/V1/Helpers/FileGenerator.cs
@@ -17,56 +17,6 @@ namespace FinancialTransactionsApi.V1.Helpers
 {
     public static class FileGenerator
     {
-        //public static byte[] WritePdfFile(List<Transaction> transactions, string name, string period, IConverter converter)
-        //{
-
-        //    var report = new ExportResponse();
-        //    var data = new List<ExportTransactionResponse>();
-
-        //    report.FullName = transactions.FirstOrDefault()?.Person?.FullName;
-        //    report.Balance = Money.PoundSterling(transactions.LastOrDefault().BalanceAmount).ToString();
-        //    report.BalanceBroughtForward = Money.PoundSterling(transactions.FirstOrDefault().BalanceAmount).ToString();
-        //    report.StatementPeriod = period;
-        //    foreach (var item in transactions)
-        //    {
-
-        //        data.Add(
-        //           new ExportTransactionResponse
-        //           {
-        //               Date = item.TransactionDate.ToString("dd MMM yyyy"),
-        //               TransactionDetail = item.TransactionSource,
-        //               Debit = Money.PoundSterling(item.PaidAmount).ToString(),
-        //               Credit = Money.PoundSterling(item.HousingBenefitAmount).ToString(),
-        //               Balance = Money.PoundSterling(item.BalanceAmount).ToString()
-        //           });
-        //    }
-        //    report.Data = data;
-        //    var globalSettings = new GlobalSettings
-        //    {
-        //        ColorMode = ColorMode.Color,
-        //        Orientation = Orientation.Portrait,
-        //        PaperSize = PaperKind.A4,
-        //        Margins = new MarginSettings { Top = 10 },
-        //        DocumentTitle = $"{name} Statement Report"
-        //    };
-        //    var objectSettings = new ObjectSettings
-        //    {
-        //        PagesCount = true,
-        //        HtmlContent = TemplateGenerator.GetHTMLReportString(report),
-        //        WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css"), LoadImages = true },
-        //        HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
-        //        FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = $"{name} Statement Report" }
-        //    };
-        //    var pdf = new HtmlToPdfDocument()
-        //    {
-        //        GlobalSettings = globalSettings,
-        //        Objects = { objectSettings }
-        //    };
-
-
-        //    var result = converter.Convert(pdf);
-        //    return result;
-        //}
         public static async Task<string> CreatePdfTemplate(IEnumerable<Transaction> transactions, string period, List<string> lines)
         {
 
@@ -76,9 +26,8 @@ namespace FinancialTransactionsApi.V1.Helpers
             model.SubFooter = lines[1];
             model.SubFooter = lines[2];
             model.Footer = lines[3];
-            // model.BankAccountNumber = string.Join(",", transactions.Select(x => x.RentAccountNumber).Distinct().ToArray());
-            model.Balance = Money.PoundSterling(transactions.LastOrDefault().BalanceAmount).ToString();
-            model.BalanceBroughtForward = Money.PoundSterling(transactions.FirstOrDefault().BalanceAmount).ToString();
+            model.Balance = (transactions == null) ? string.Empty : Money.PoundSterling(transactions.LastOrDefault().BalanceAmount).ToString();
+            model.BalanceBroughtForward = (transactions == null) ? string.Empty : Money.PoundSterling(transactions.FirstOrDefault().BalanceAmount).ToString();
             model.StatementPeriod = period;
             foreach (var item in transactions)
             {
