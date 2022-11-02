4d1b30acfe592deeb5a187b1e964a905f5125ff0
DESKTOP-KJNO7KJ\Ilko Adamov <ilko.z.adamov@gmail.com>
2022-11-02 14:24:14 +0200

    feature/HFS-615 - Add pagination: missing reference

--- FinancialTransactionsApi/V1/Factories/ResponseFactory.cs
+++ FinancialTransactionsApi/V1/Factories/ResponseFactory.cs
@@ -3,6 +3,7 @@ using FinancialTransactionsApi.V1.Domain;
 using System.Collections.Generic;
 using System.Linq;
 using FinancialTransactionsApi.V1.Helpers;
+using FinancialTransactionsApi.V1.Helpers.GeneralModels;
 
 namespace FinancialTransactionsApi.V1.Factories
 {
