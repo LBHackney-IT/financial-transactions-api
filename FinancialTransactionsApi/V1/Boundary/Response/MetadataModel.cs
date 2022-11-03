24ecce0ec6ba439786a5325964696163ccedc70b
Geraint <geraint87@gmail.com>
2022-11-03 08:52:49 +0000

    Remove extra line

--- FinancialTransactionsApi/V1/Boundary/Response/MetadataModel.cs
+++ FinancialTransactionsApi/V1/Boundary/Response/MetadataModel.cs
@@ -5,7 +5,6 @@ namespace FinancialTransactionsApi.V1.Boundary.Response
     public class MetadataModel
     {
         [JsonProperty("pagination", NullValueHandling = NullValueHandling.Ignore)]
-
         public Pagination Pagination { get; set; }
     }
 }
